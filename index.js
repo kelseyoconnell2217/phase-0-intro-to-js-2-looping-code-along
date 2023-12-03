dafe5546e8d20b3cdcfdb5638f41c007db2d8697
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-03 18:12:25 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [] 
+    for(let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`) 
+    }
+    return messages
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown() {
+    let countDown = 10
+    while(countDown >= 0) {
+        console.log(countDown)
+        countDown = countDown - 1
+    }
+}
