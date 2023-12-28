cf8edf6ee41b4a2c97aef7b1978ef777c66ade89
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-28 13:06:07 -0500

    Good to go

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-function writeCards(names, event) {
-    const messages = [] 
-    for(let i = 0; i < names.length; i++) {
-       messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`) 
+function writeCards(names, event){
+    const cards = []
+    for(let i = 0; i < names.length; i++){
+       cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
     }
-    return messages
-}
+    return cards
+} 
 console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
 
-function countDown() {
-    let countDown = 10
-    while(countDown >= 0) {
-        console.log(countDown)
-        countDown = countDown - 1
+function countDown(x){
+    while (x>=0){
+        console.log(x);
+        x = x-1
     }
 }
